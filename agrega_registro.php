746e61c90d248144880c556106857a6108d01976
Salvador Sastré <chavosastre@gmail.com>
2019-04-19 10:16:21 -0500

    Actualizacion formato

--- agrega_registro.php
+++ agrega_registro.php
@@ -15,6 +15,7 @@
 	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
 
@@ -23,22 +24,81 @@
         // if(isset($_POST['btenviar']))
         // {
             $arregloCHK = $_POST['chk'];
-            $num = count($arregloCHK);
+			$num = count($arregloCHK);
+
+			switch (date("m")) {
+				case '01':
+					$mes = "enero";
+					break;
+				case '02':
+					$mes = "febrero";
+					break;
+				case '03':
+					$mes = "marzo";
+					break;
+				case '04':
+					$mes = "abril";
+					break;
+				case '05':
+					$mes = "mayo";
+					break;
+				case '06':
+					$mes = "junio";
+					break;
+				case '07':
+					$mes = "julio";
+					break;
+				case '08':
+					$mes = "agosto";
+					break;
+				case '09':
+					$mes = "septiembre";
+					break;
+				case '10':
+					$mes = "octubre";
+					break;
+				case '11':
+					$mes = "noviembre";
+					break;
+				case '12':
+					$mes = "diciembre";
+					break;
+			}
             // print_r('Total Checkbox Seleccionados: '.$num.'</br>');
             // print_r('Valores: </br>');
 
             for($n = 0; $n < $num; $n++)
             {
-                print_r($arregloCHK[$n].'</br>');
+                // print_r($arregloCHK[$n].'</br>');
             }
         // }
     ?>
-
+	<form action="guarda_registro.php" method="post">
 			<div class="container">
-				<div class="class">
-					
+				<div class="row col-xs-12 col-sm-12 col-md-12">
+					<div class="col-xs-3 col-sm-3 col-md-3 text-center">
+						<img class="logoPDF" src="images/ecopulse.png" alt="">
+					</div>
+					<div class="col-xs-9 col-sm-9 col-md-9">
+						<p class="izq">Mérida Yucatán <?php echo "a " . date("d") . " de " . $mes . " de " . date("Y"); ?></p>
+					</div>
+				</div>
+				<div class="row col-xs-12 col-sm-12 col-md-12">
+					<div class="col-xs-3 col-sm-3 col-md-3">
+					<p class="piepag">
+						contacto@ecopulse.mx</br>
+						www.ecopulse.mx</br>
+						</br>
+						Calle 56 num. 551 depto. 4 por 79 y 81 Col.</br>
+						Núcleo Dzodzil C.P. 97300 Mérida,</br>
+						Yucatán, México. T. +52 999 688 2513</br>
+					</p>
+					</div>
+					<div class="col-xs-9 col-sm-9 col-md-9 text-center p-t-30">
+						<h1><strong>RESGUARDO DE EQUIPO Y HERRAMIENTAS</strong></h1>
+					</div>
 				</div>
 			</div>
-
+	</form>
 </body>
 </html>
